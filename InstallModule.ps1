6dd52aa61960b04bc19c753fd8584373cb0a4a50
Julien Mazoyer <xephren@gmail.com>
2019-11-14 23:04:27 +0100

    ScriptInstaller

---
+++ InstallModule.ps1
@@ -0,0 +1,28 @@
+﻿$ZipURL = "https://github.com/Reyozam/JM.ScriptTools/archive/master.zip"
+$ModuleName = "JM.ScriptTools"
+#=====================================================================================
+$DownloadFile = Join-Path $env:TEMP $($ModuleName + ".zip")
+$TempDir = Join-Path $env:TEMP -ChildPath (New-Guid).Guid
+
+if ($PSVersionTable.PSVersion.Major -gt 5)
+{
+    $Destination = "C:\Program Files\PowerShell\Modules\$ScriptName"
+}
+else
+{
+    $Destination = "C:\Program Files\WindowsPowerShell\Modules\$ScriptName"
+}
+
+Write-Verbose "Download of $ZipURL ..." -Verbose
+Invoke-WebRequest -Uri $ZipURL -OutFile $DownloadFile
+Unblock-File $DownloadFile
+
+Write-Verbose "Unzip $DownloadFile" -Verbose
+Expand-Archive -Path $DownloadFile -DestinationPath $TempDir -Force
+$UnzippedFolder = (Get-ChildItem $TempDir | Where-Object {$_.Name -like "*-master"}).FullName 
+
+
+Write-Verbose "Copy to $Destination ..."
+
+Copy-Item -Path "$($UnzippedFolder)\" -Destination $Destination -Verbose -Recurse
+
