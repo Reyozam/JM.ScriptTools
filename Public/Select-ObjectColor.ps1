0577e5cfaf11b531ddc3bdc6e2c96af002614463
Reyozam <julien@mazoyer.me>
2022-02-04 15:32:53 +0100

    2022.02.04

---
+++ Public/Select-ObjectColor.ps1
@@ -0,0 +1,54 @@
+﻿function Select-ObjectColor
+{
+    [CmdletBinding()]
+    param (
+        # Parameter help description
+        [Parameter(ValueFromPipeline = $true)]
+        [Object[]]$InputObject,
+
+        [Parameter(Mandatory = $true)]
+        [string[]]$SelectedProperty,
+
+        [Parameter(Mandatory = $true)]
+        [string]$ColoredProperty,
+
+        [Parameter()]
+        [string]$SuccessMatch,
+
+        [Parameter()]
+        [string]$WarningMatch,
+
+        [Parameter()]
+        [string]$ErrorMatch
+
+    )
+
+    process
+    {
+
+        $IndexOf = $SelectedProperty.IndexOf($ColoredProperty)
+        $ArrayBefore = $SelectedProperty[0..($IndexOf - 1)]
+        $ArrayAfter = $SelectedProperty[$($IndexOf + 1)..$($SelectedProperty.Count)]
+    
+    
+        $ColoredColumn = @{
+            Name       = $ColoredProperty
+            Expression =
+            {
+                switch ($_.$ColoredProperty)
+                {
+                    "$SuccessMatch" { $color = "32"; break }
+                    "$WarningMatch" { $color = '33'; break }
+                    "$ErrorMatch"   { $color = "31"; break }
+                    default { $color = "0" }
+                }
+                $e = [char]27
+                "$e[${color}m$($_.$ColoredProperty)${e}[0m"
+            }
+            
+        }
+       
+        $hash = @{ Property = $ArrayBefore + $ColoredColumn + $ArrayAfter }
+        $InputObject | Select-Object @hash
+    }
+}
--- Public/Write-Log.ps1
+++ Public/Write-Log.ps1
@@ -1,94 +1,60 @@
-﻿function Write-Log
+﻿Function Write-Log
 {
+
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory = $false, ParameterSetName = "StartLog", ValueFromPipeline = $true)][switch]$StartLog,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = "StartLog")][string]$LogFile,
-        [Parameter(Position = 0, Mandatory = $true, ParameterSetName = "Message", ValueFromPipeline = $true)][string]$Message,
-        [Parameter(Mandatory = $false, ParameterSetName = "Message", ValueFromPipeline = $false)][ValidateSet('Info', 'Warning', 'Error')][string]$Level = "Info",
-        [Parameter(Mandatory = $false, ParameterSetName = "EndLog", ValueFromPipeline = $true)][switch]$EndLog,
-        [Parameter(Mandatory = $false, ParameterSetName = "Message", ValueFromPipeline = $true)][int]$Tab,
-        [Parameter(Mandatory = $false, ParameterSetName = "Message", ValueFromPipeline = $true)][int]$TimeStamp
-        
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)][string]$Message,
+        [Parameter(Position = 0, Mandatory = $false, ValueFromPipeline = $true)][string]$Context,
+        [Parameter(Mandatory = $false)][ValidateSet('Info', 'Warning', 'Error', 'Success')][string]$Level = "Info",
+        [Parameter(Mandatory = $false)][ValidateSet('Before', 'After')][string]$JumpLine,
+        [Parameter(Mandatory = $false)][int]$Tab = 0,
+        [Parameter(Mandatory = $false)][string]$LogFile,
+        [Parameter(Mandatory = $false)][switch]$ClearLog
     )
 
-    #--------------------------------------------=
-    # EXECUTION
-    #--------------------------------------------=
-
-    Switch ($PsCmdlet.ParameterSetName)
+    switch ($Level)
     {
-        "StartLog"
+        Info
         {
-            $script:StartDate = Get-Date
-            $User = [Security.Principal.WindowsIdentity]::GetCurrent()
-            $Elevated = ([Security.Principal.WindowsPrincipal]$User).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-
-            if ($PSVersionTable.PSVersion -gt "4.0") { $CIM = Get-CimInstance win32_operatingsystem -Property Caption, Version, OSArchitecture }
-            else { $CIM = Get-WmiObject win32_operatingsystem -Property Caption, Version, OSArchitecture }
-
-            $Header = "------------------------------------------------------------------------------------------`r`n"
-            $Header += "Script Name        : $($myinvocation.ScriptName)  `r`n"
-            $Header += "Generated on       : $($StartDate.tostring("dd/MM/yy HH:mm:ss")) `r`n"
-            $Header += "User               : $($user.Name) `r`n"
-            $Header += "Elevated           : $Elevated`r`n"
-            $Header += "Computer           : $ENV:COMPUTERNAME`r`n"
-            $Header += "OS                 : $($CIM.Caption) [Build :$($CIM.Version)] `r`n"
-            $Header += "PS Version         : $($PSVersionTable.PSVersion)`r`n"
-            $Header += "LogFile            : $LogFile`r`n"
-            $Header += "------------------------------------------------------------------------------------------`n"
-
-                if (-not(Test-Path $LogFile)) { [void](New-Item $LogFile -Force) }
-                $script:LogFile = $LogFile
-
-                Write-Output $Header | Set-Content $script:Logfile -PassThru -Encoding unicode
+            $Label = @{Log = "[INFO]" ; Icon = "   " ; Color = "White" }
+                    
         }
-
-        "Message"
+        Error
         {
-
-            $BCKColor = $Host.ui.RawUI.ForegroundColor
-
-            $Line = [System.Text.StringBuilder]::new()
-
-            if ($TimeStamp){[void]$Line.Append("[$([datetime]::Now.ToString("HH:mm:ss:fff"))]")}
-            #[void]$Line.Append([string]::Format(" [{0,-7}] ", $Level.ToUpper()))
-            [void]$Line.Append("   " * $Tab)
-
-            switch ($Level)
-            {
-                Info    { [void]$Line.Append("[i] ") ; $Host.ui.RawUI.ForegroundColor = "White"      }
-                Error   { [void]$Line.Append("[x] ") ; $Host.ui.RawUI.ForegroundColor = "Red"        }
-                Warning { [void]$Line.Append("[!] ") ; $Host.ui.RawUI.ForegroundColor = "DarkYellow" }
-            }
-
-            [void]$Line.Append($Message)
-
-            Write-Output $Line.ToString() | Add-Content $script:Logfile -PassThru -Encoding unicode
-            $host.ui.RawUI.ForegroundColor = $BCKColor
-
-        
+            $Label = @{Log = "[ERROR]" ; Icon = "[x]" ; Color = "Red" }
         }
-
-        "EndLog"
+        Warning
         {
-            $EndDate = Get-Date
-            $TimeSpan = New-TimeSpan -Start $StartDate -End $EndDate
-
-            $Footer += "`r`n"
-            $Footer += "+----------------------------------------------------------------------------------------+`r`n"
-            $Footer += "End Time                 : $EndDate`r`n"
-            $Footer += "Total Duration           : $($TimeSpan.ToString()) `r`n"
-            $Footer += "+----------------------------------------------------------------------------------------+"
-
-            Write-Output $Footer | Add-Content $script:Logfile -PassThru -Encoding unicode
+            $Label = @{Log = "[WARNING]" ; Icon = "[!]" ; Color = "DarkYellow" }
         }
+        Success
+        {
+            $Label = @{Log = "[SUCCESS]" ; Icon = "[+]" ; Color = "Green" }
+        }
+    }
+    
+    $TabString = "  "
+    if ($Tab -gt 0) { $Indent = "{0}|{1}" -f $($TabString * ($Tab - 1)), $TabString }
+    else { $Indent = "" }
 
+    if ($Context) { $Context = "[" + $Context + "]" }
+            
+    if ($JumpLine -eq "Before" ) { Write-Host }
+    Write-Host  $( "{0,-4} {3,-7} {1}{2}" -f $Label["Icon"], $Indent, $Message, $Context ) -ForegroundColor $Label["Color"]
+    if ($JumpLine -eq "After" ) { Write-Host }
+
+    #LOG
+    if ( $LogFile )
+    {
+        if (!(Test-Path $LogFile)) { New-Item -Path $LogFile -ItemType File -Force | Out-Null }
+
+        $OutFileParams = @{ FilePath = $LogFile  ; Encoding = "utf8" }
+        if (-not $ClearLog) { $OutFileParams["Append"] = $true }
+
+
+        if ($JumpLine -eq "Before" ) { " " | Out-File @OutFileParams }
+        "{0,-21}{1,-10} {4,-7} {2}{3}" -f (Get-Date -Format 'yyyy-MM-dd HH:mm:ss'), $Label["Log"], $Indent, $Message, $Context | Out-File @OutFileParams
+        if ($JumpLine -eq "After" ) { " "  | Out-File @OutFileParams }
     }
 
-}
-
-
-
-
-
+} 
