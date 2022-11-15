491fda9f2a408e96814624eed012d5725cc12749
Reyozam <julien@mazoyer.me>
2022-11-15 08:57:51 +0100

    Write-Log Rework

--- Public/Write-Log.ps1
+++ Public/Write-Log.ps1
@@ -5,56 +5,62 @@
     Param (
         [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)][string]$Message,
         [Parameter(Position = 0, Mandatory = $false, ValueFromPipeline = $true)][string]$Context,
-        [Parameter(Mandatory = $false)][ValidateSet('Info', 'Warning', 'Error', 'Success')][string]$Level = "Info",
+        [Parameter(Mandatory = $false)][ValidateSet('Info', 'Warning', 'Error', 'Success')][string]$Level = 'Info',
         [Parameter(Mandatory = $false)][ValidateSet('Before', 'After')][string]$JumpLine,
+        [Parameter(Mandatory = $false)][switch]$TimeStamp,
         [Parameter(Mandatory = $false)][int]$Tab = 0,
         [Parameter(Mandatory = $false)][string]$LogFile,
         [Parameter(Mandatory = $false)][switch]$ClearLog
     )
 
+    $ESC = [char]27
+    $String = [System.Text.StringBuilder]::new()
+
+    if ($TimeStamp) { [void]$String.Append("$ESC[90m$(Get-Date -f "[HH:mm:ss]")$ESC[0m ")  }
+
     switch ($Level)
     {
-        Info
-        {
-            $Label = @{Log = "[INFO]" ; Icon = "   " ; Color = "White" }
-                    
+        Info {
+            [void]$String.Append("$ESC[37m$("   ")$ESC[0m ")
+            if ($Tab -gt 0) { [void]$String.Append("   ")  }
+            if ($Context) { [void]$String.Append("$ESC[36m$("[$Context]")$ESC[0m ") }
+            [void]$String.Append("$ESC[37m$($Message)$ESC[0m ")
         }
-        Error
-        {
-            $Label = @{Log = "[ERROR]" ; Icon = "[x]" ; Color = "Red" }
+        Error {
+            [void]$String.Append("$ESC[31m$("x ")$ESC[0m ")
+            if ($Tab -gt 0) { [void]$String.Append("   ")  }
+            if ($Context) { [void]$String.Append("$ESC[36m$("[$Context]")$ESC[0m ") }
+            [void]$String.Append("$ESC[31m$($Message)$ESC[0m ")
         }
-        Warning
-        {
-            $Label = @{Log = "[WARNING]" ; Icon = "[!]" ; Color = "DarkYellow" }
+        Warning {
+            [void]$String.Append("$ESC[33m$("! ")$ESC[0m ")
+            if ($Tab -gt 0) { [void]$String.Append("   ")  }
+            if ($Context) { [void]$String.Append("$ESC[36m$("[$Context]")$ESC[0m ") }
+            [void]$String.Append("$ESC[33m$($Message)$ESC[0m ")
         }
-        Success
-        {
-            $Label = @{Log = "[SUCCESS]" ; Icon = "[+]" ; Color = "Green" }
+        Success {
+            [void]$String.Append("$ESC[32m$("+ ")$ESC[0m ")
+            if ($Tab -gt 0) { [void]$String.Append("   ")  }
+            if ($Context) { [void]$String.Append("$ESC[36m$("[$Context]")$ESC[0m ") }
+            [void]$String.Append("$ESC[32m$($Message)$ESC[0m ")
         }
     }
-    
-    $TabString = "  "
-    if ($Tab -gt 0) { $Indent = "{0}|{1}" -f $($TabString * ($Tab - 1)), $TabString }
-    else { $Indent = "" }
 
-    if ($Context) { $Context = "[" + $Context + "]" }
-            
-    if ($JumpLine -eq "Before" ) { Write-Host }
-    Write-Host  $( "{0,-4} {3,-7} {1}{2}" -f $Label["Icon"], $Indent, $Message, $Context ) -ForegroundColor $Label["Color"]
-    if ($JumpLine -eq "After" ) { Write-Host }
+    if ($JumpLine -eq 'Before' ) { Write-Output "" }
+    $String.ToString()
+    if ($JumpLine -eq 'After' ) { Write-Output "" }
 
     #LOG
     if ( $LogFile )
     {
         if (!(Test-Path $LogFile)) { New-Item -Path $LogFile -ItemType File -Force | Out-Null }
 
-        $OutFileParams = @{ FilePath = $LogFile  ; Encoding = "utf8" }
-        if (-not $ClearLog) { $OutFileParams["Append"] = $true }
+        $OutFileParams = @{ FilePath = $LogFile  ; Encoding = 'utf8' }
+        if (-not $ClearLog) { $OutFileParams['Append'] = $true }
 
-
-        if ($JumpLine -eq "Before" ) { " " | Out-File @OutFileParams }
-        "{0,-21}{1,-10} {4,-7} {2}{3}" -f (Get-Date -Format 'yyyy-MM-dd HH:mm:ss'), $Label["Log"], $Indent, $Message, $Context | Out-File @OutFileParams
-        if ($JumpLine -eq "After" ) { " "  | Out-File @OutFileParams }
+        if ($JumpLine -eq 'Before' ) { ' ' | Out-File @OutFileParams }
+        '{0,-21}[{1,-10}] [{2}] {3}{4}' -f (Get-Date -Format 'yyyy-MM-dd HH:mm:ss'), $Level.ToUpper() , $Context  , $Indent, $Message | Out-File @OutFileParams
+        if ($JumpLine -eq 'After' ) { ' '  | Out-File @OutFileParams }
     }
 
-} 
+}
